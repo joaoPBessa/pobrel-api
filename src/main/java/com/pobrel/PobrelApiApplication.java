c53d5fdfb9dfb75144c29089927151f05c419f31
João Pedro Bessa da Silva <joao.bessa.12@gmail.com>
2021-12-27 21:52:41 -0300

    Project structure

---
+++ src/main/java/com/pobrel/PobrelApiApplication.java
@@ -0,0 +1,13 @@
+package com.pobrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PobrelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PobrelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pobrel/PobrelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pobrel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PobrelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
